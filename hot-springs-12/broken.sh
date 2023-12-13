f1280ac13e8015b6402fc1c2b0655279029009e6
Maximilian Mühlfeld <maggistro@googlemail.com>
2023-12-13 22:03:10 +0100

    Broken trys

---
+++ hot-springs-12/broken.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+debug=${1:0}
+readarray -t lines < hot-springs-12/miniinput
+
+getAllMaskVariations () {
+    local groupDefinition=$1
+    local groupSize=$2
+    local max_pos=-1
+
+    local -A hitList=()
+    for (( i=0; i<${#groupDefinition[@]} - $groupSize; i++ )); do
+        if [[ $i -eq $max_pos ]]; then
+            break
+        fi
+        hit=0
+        for (( pos=0; pos<$groupSize; pos++)); do
+            case ${groupDefinition[i]} in
+                '#') 
+                    hit=$((hit + 1)) 
+                    ;;
+            esac
+        done
+        hitList[$i]=$hit
+
+        #natural hit, all ? must be 0
+        if [[ $hit -eq $groupSize ]]; then
+            echo 1
+            return
+        fi
+
+        #clear hitlist to always include #
+        if [[ $hit -ge 1 ]] && [[ $max_pos -eq -1 ]]; then
+            hitList=()
+            max_pos=$((pos + 2*groupSize - 2))    
+        fi
+    done
+
+    echo ${#hitList[@]}
+}
+
+createMaskList () {
+    local -n possible_masks=$1
+    IFS=',' read -r -a temp_groups <<< "$2"
+    local inputLength=$3
+
+    mask=''
+
+    for group in "${groups[@]}"; do
+        for (( i=0; i<${#group}; i++ )); do
+            mask+='1'
+        done
+        mask+='0'
+    done
+
+    getAllMaskVariations $mask $inputLength possible_masks
+}
+
+checkMatch () {
+    local maskPart=$1
+    local groupSize=$2
+
+    for (( one=0; one<${#groupSize[@]}; one++ )); do
+        if [[ ${maskPart[$one]} -eq '.' ]]; then
+            echo 0
+            return
+        fi
+    done
+
+    if [[ ${maskPart:$groupSize:1} -eq '#' ]]; then
+        echo 0
+        return
+    fi
+
+    echo 1
+}
+
+matchGroupCount () {
+    local mask=$1
+    shift
+    local groups=( "$@" )
+
+    local group_number=0
+    local variant=0
+
+    for (( pos=0; pos<${#mask}; pos++ )); do
+        if [[ $(checkMatch ${mask:$pos:$((size + 1))} ${groups[$group_number]}) -eq 1 ]]; then
+            newVariants=$(matchGroupCount ${mask:$((pos+1))} "${groups[@]:1}")
+            variant=$((newVariants + variant))
+        fi
+    done
+
+    echo $variant
+}
+
+variations=0
+for line in "${lines[@]}"; do
+    IFS=' ' read -r -a maskAndGroups <<< "${line}"
+
+    variations=$((variations + $(matchGroupCount ${maskAndGroups[0]} ${maskAndGroups[1]})))
+done
+
+echo $variations
--- mirrors-13/find_mirrors.sh
+++ mirrors-13/find_mirrors.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
 debug=${1:0}
-readarray -t lines < mirrors-13/input
+readarray -t lines < ./mirrors-13/miniinput
 
 
 check_vertical_mirror () {
-    local input=$1
+    local ignore_left=$1
+    local input=$2
     local length=${#input}
     local pos=0
 
-    [[ $debug -ge 2 ]] && echo "entry: $input" > /dev/tty
+    [[ $debug -ge 3 ]] && echo "entry: $input" > /dev/tty
 
     #check even
     if [[ $((length % 2)) -eq 0 ]]; then
-        if [[ ${input:0:$((length/2))} == $(rev <<< ${input:$((length/2))}) ]]; then
+        # [[ $length -eq 8 ]] &&  echo $input > /dev/tty
+        [[ $length -eq 8 ]] &&  echo $input ${input:0:$((length/2))} $(rev <<< "${input:$((length/2))}") > /dev/tty
+        if [[ ${input:0:$((length/2))} == $(rev <<< "${input:$((length/2))}") ]]; then
             echo $((length/2))
         else
             echo -1
         fi
     else 
         # cut front
-        if [[ "${input:1:$((length/2))}" == $(rev <<< ${input:$((length/2 + 1))}) ]]; then
+        if [[ $ignore_left -eq 1 && "${input:1:$((length/2))}" == $(rev <<< ${input:$((length/2 + 1))}) ]]; then
             echo $((length/2 + 1))
         # cut back
-        elif [[ ${input:0:$((length/2))} == $(rev <<< ${input:$((length/2)):$((length/2))}) ]]; then
+        elif [[ $ignore_left -eq 0 && ${input:0:$((length/2))} == $(rev <<< ${input:$((length/2)):$((length/2))}) ]]; then
             echo $((length/2))
         else           
             echo -1
@@ -34,51 +37,86 @@ check_vertical_mirror () {
 flip () {
     local input=( "$@" )
     local length=${#input[0]}
-    local height=${#input}
+    local height=${#input[@]}
     local temp=()
 
     for (( i=0; i<length; i++ )); do
-        temp+=( '' )
         for (( j=0; j<height; j++ )); do
-            temp[i]=${temp[i]}${input[$j]:$i:1}
+            line=${input[$j]}
+            temp[$i]=${temp[$i]}${line:$i:1}
         done
     done
 
     echo "${temp[@]}"
 }
 
+print_block () {
+    local -n data=$1
+    for entry in "${data[@]}"; do
+        echo $entry > /dev/tty
+    done
+}
+
+check_subset () {
+    local pos=$1
+    local length=$2
+    local -n subset=$3
+    
+    for set in "${subset[@]}"; do
+        if [[ ${set:$pos:$((length/2))} != $(rev <<< "${set:$((pos + length/2))}") ]]; then
+            echo 0
+            return
+        fi
+    done
+
+    echo 1
+}  
+
 check_block () {
     local -n block=$1
 
-    [[ $debug -ge 2 ]] && echo "block: ${block[@]}" > /dev/tty
+    # [[ $debug -ge 2 ]] && print_block block
 
     local pos=-1
     # check vertical
     for entry in "${block[@]}"; do
-        pos=$(check_vertical_mirror $entry)
-        if [[ $pos -eq -1 ]]; then
-            [[ $debug -ge 2 ]] && echo "no vertical mirror" > /dev/tty
-            break
-        fi
+        # ignore right
+        for (( i=${#entry}; i>0; i-- )); do
+            newPos=$(check_vertical_mirror 0 ${entry:0:$i})
+            pos=$(( newPos > pos ? newPos : pos ))
+        done
     done
+
+    [[ $debug -ge 2 ]] && echo "checked left" > /dev/tty
+    if [[ $pos -ne -1 ]]; then
+        length=$((pos * 2))
+        if [[ $(check_subset $pos $length block) -eq 1 ]]; then
+            echo $pos
+            return 
+        fi
+    fi
+    [[ $debug -ge 2 ]] && echo "continue" > /dev/tty
+
+    pos=1000000
+    for entry in "${block[@]}"; do
+        # ignore left
+        for (( i=0; i<${#entry}; i++ )); do
+            newPos=$(check_vertical_mirror 1 ${entry:$i})
+            pos=$(( newPos < pos ? newPos : pos ))
+        done
+    done
+    [[ $debug -ge 2 ]] && echo "checked right $pos" > /dev/tty
 
     if [[ $pos -ne -1 ]]; then
-        echo $pos
-        return
-    fi
-
-    block=( $(flip "${block[@]}") )
-    # check horizontal
-
-    for entry in "${block[@]}"; do
-        pos=$(check_vertical_mirror $entry)
-        if [[ $pos -eq -1 ]]; then
-            [[ $debug -ge 2 ]] && echo "no horizontal mirror" > /dev/tty
-            break
+        length=$(( (${#entry} - pos) * 2 ))
+        if [[ $(check_subset $pos $length  block) -eq 1 ]]; then
+            echo $pos
+            return
         fi
-    done
+    fi
+    [[ $debug -ge 2 ]] && echo "continue" > /dev/tty
 
-    echo $((pos * 100))
+    echo 0;
 }
 
 temp=()
@@ -86,7 +124,15 @@ linePos=0
 sum=0
 for (( linePos=0; linePos<${#lines[@]}; linePos++ )); do
     if [[ ${lines[$linePos]} == '' ]]; then
-        sum=$((sum + $(check_block temp)))
+        # check vertical
+        result=$(check_block temp)
+        if [[ $result -eq 0 ]]; then
+            flipped=( $(flip "${temp[@]}") )
+            
+            [[ $debug -ge 2 ]] && echo "check lines" > /dev/tty
+            result=$(( $(check_block flipped) * 100 ))
+        fi
+        sum=$((sum + result))
         temp=()
         echo $sum > /dev/tty
     else
